898db8c6bb418d37f37d96c9ff824fca94e8d1dd
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-09-20 19:35:49 +0500

    Create SavingAccount.java

---
+++ BankingSystem_Interfa_Classes/src/SavingAccount.java
@@ -0,0 +1,8 @@
+public class SavingAccount  {
+    String  name ;
+
+   void  SavingAccount (String name){
+        this.name = name;
+        System.out.println("Account Created for "+name+" !");
+    }
+}
